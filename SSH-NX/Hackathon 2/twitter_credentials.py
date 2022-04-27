1effdf6a5f404c7692cd89ef8f8f91c24aa2b90c
hpmouton <hubertthedev@gmail.com>
2022-04-27 10:31:58 +0200

    ssh-nx

---
+++ SSH-NX/Hackathon 2/twitter_credentials.py
@@ -0,0 +1,8 @@
+
+# Variables which contain the user credentials needed to access the Twitter API
+# Copy and paste your credentials within the quotation marks '' as these are string variables
+
+api_key = 'ZmtpNLVWvJ5A7uDnNk4jlNiAG'
+api_secret_key =  'X1qXatt8RJHIzP586t91jwL4Mpi744sD00KFEem6UczEVibTTP'
+access_token = '1111544057275342850-VQw0ntbKvDCQEvW98GCaVmDg9LANRC'
+access_token_secret = 'l3va7uBhggdMP4nvfit5rKlk5MkQ10ApRAiGsQF21NYIr'
