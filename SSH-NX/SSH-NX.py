4688de0e8005714285ba71f809ca390c1145be36
hpmouton <hubertthedev@gmail.com>
2022-04-29 00:26:48 +0200

    initial app

--- SSH-NX/SSH-NX.py
+++ SSH-NX/SSH-NX.py
@@ -12,7 +12,7 @@ st.sidebar.title('Sentiment analysis of airlines')
 st.sidebar.markdown("🛫We can analyse passengers review from this application.🛫")
 #loading the data (the csv file is in the same folder)
 #if the file is stored the copy the path and paste in read_csv method.
-data=pd.read_csv('Hackathon 2\external_labelled_tweets_clean.csv')
+data=pd.read_csv('Hackathon 2/external_labelled_tweets_clean.csv')
 #checkbox to show data 
 if st.checkbox("Show Data"):
     st.write(data.head(50))
@@ -20,9 +20,9 @@ if st.checkbox("Show Data"):
 st.sidebar.subheader('Tweets Analyser')
 #radio buttons
 tweets=st.sidebar.radio('Sentiment Type',('positive','negative','neutral'))
-st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
-st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
-st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(data.query('VADERsentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(data.query('VADERsentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(data.query('VADERsentiment==@tweets')[['text']].sample(1).iat[0,0])
 #selectbox + visualisation
 # An optional string to use as the unique key for the widget. If this is omitted, a key will be generated for the widget based on its content.
 ## Multiple widgets of the same type may not share the same key.
